ab53ff9c0af8c9d383351198c8d6d36ad982cebe
Iman Asgari <imanunkiller10102020@gmail.com>
2022-01-21 11:03:20 +0330

    mounting data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json)
+      .then((json) =>
+        this.setState({
+          items: json,
+          isLoaded: true,
+        })
+      );
+  }
+
   render() {
-    return (
-      <div>
-        <h1>React App!</h1>
-      </div>
-    );
+    var { items, isLoaded } = this.state;
+
+    if (!isLoaded) {
+      return <div>Data is Loading....</div>;
+    } else {
+      return (
+        <div className="App">
+          <h1>Data has been Loaded</h1>
+        </div>
+      );
+    }
   }
 }
